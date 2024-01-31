bb47b66966490c436e63b61ef9df81e483883b5a
Sambir <bshtangret24@gmail.com>
2024-01-31 01:10:30 +0100

    asd

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ import typescript from "rollup-plugin-typescript2"
 import postcss from "rollup-plugin-postcss"
 import resolve from '@rollup/plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
+import cssnano from 'cssnano'
 
 export default {
   input: "src/index.ts",
@@ -19,12 +20,17 @@ export default {
       sourcemap: false
     }
   ],
+  treeshake: true,
   plugins: [
     peerDepsExternal(),
     resolve(),
     typescript({ tsconfig: './tsconfig.json' }),
     commonjs(),
-    postcss(),
+    postcss({
+      plugins: [
+        cssnano(),
+      ],
+    }),
     terser()
   ]
 }
